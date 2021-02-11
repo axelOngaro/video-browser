38cb18031557c7fd2d7df38e57227dd30343fd10
Axel <axel.ongaro.pro@gmail.com>
2021-02-11 17:38:38 +0100

    Added API call and videolist component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange" />
+    <VideoList />
+    {{ videos.length }}
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+const API_KEY = "AIzaSyAyxijaTTkp_6vYVpAOewqIIC-mD0tFH1Q";
+
 export default {
   name: "App",
   components: {
     SearchBar,
+    VideoList,
+  },
+  data() {
+    return {
+      videos: [],
+    };
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((res) => {
+          this.videos = res.data.items;
+        });
     },
   },
 };
---
+++ src/components/VideoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    Videolist
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "VideoList",
+};
+</script>
+
+<style lang="stylus" scoped></style>
