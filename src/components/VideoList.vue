656840bc5a0a65e13ae7a726de6f5fe2526ceb59
Axel <axel.ongaro.pro@gmail.com>
2021-02-11 19:36:00 +0100

    Added VideoListItem and list iteration

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,19 @@
 <template>
   <ul>
-    Videolist
+    <VideoListItem v-for="video in videos" :key="video.etag" :video="video" />
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem,
+  },
+  props: {
+    videos: Array,
+  },
 };
 </script>
 
